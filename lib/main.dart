561d8df57ec52d8ca7178af0d0ffe22708a8b016
Toby de Mendonca <75569552+tobiasdemendonca@users.noreply.github.com>
2024-09-17 15:33:06 +0100

    Completed section 2

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,14 @@
 import 'package:flutter/material.dart';
+import 'package:udemy_1013o4/gradient_container.dart';
 
 void main() {
-  runApp(const MaterialApp(home: Scaffold(body: Text('Hello world'))));
+  runApp(
+    const MaterialApp(
+      home: Scaffold(
+          backgroundColor: Color.fromARGB(255, 173, 192, 190),
+          body: GradientContainer(
+            [Color.fromARGB(255, 219, 179, 226), Colors.blueGrey],
+          )),
+    ),
+  );
 }
