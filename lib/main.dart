958ae3c9fa0f279d55c285633e662c47bbfdeaa4
Toby de Mendonca <75569552+tobiasdemendonca@users.noreply.github.com>
2024-09-17 17:22:17 +0100

    End of step 53 own attempt

--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,12 @@
 import 'package:flutter/material.dart';
-import 'package:udemy_1013o4/gradient_container.dart';
+import 'package:udemy_1013o4/start_container.dart';
 
 void main() {
   runApp(
     const MaterialApp(
       home: Scaffold(
-          backgroundColor: Color.fromARGB(255, 173, 192, 190),
-          body: GradientContainer(
-            [Color.fromARGB(255, 219, 179, 226), Colors.blueGrey],
-          )),
+        body: StartContainer(),
+      ),
     ),
   );
 }
